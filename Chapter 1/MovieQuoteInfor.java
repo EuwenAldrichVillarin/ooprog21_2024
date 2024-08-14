855bfb3d815a303c887427b882baf60d2483659a
EuwenAldrichVillarin <euwenaldrich123@gmail.com>
2024-08-14 22:08:26 +0800

    Update MovieQuoteInfor.java

--- Chapter 1/MovieQuoteInfor.java
+++ Chapter 1/MovieQuoteInfor.java
@@ -1,1 +1,12 @@
+public class MovieQuoteInfor{
+    public static void main(String[]args){
+        System.out.println("\"The world ain't all sunshine and rainbows. It's a very mean and nasty place, and I don't care how");
+        System.out.println("tough you are, it will beat you to your knees and keep you there permanently if you let it. You, me, or");
+        System.out.println("nobody is gonna hit as hard as life. But it ain't about how hard you hit. It's about how hard you can get");
+        System.out.println("hit and keep moving forward; how much you can take and keep moving forward. That's how winning is done!\"");
+        System.out.println("Movie:Rocky Balboa");
+        System.out.println("Said by:Rocky");
+        System.out.println("Year:2006");
+    }
+}
 
