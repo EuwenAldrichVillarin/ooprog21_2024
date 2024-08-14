da559472a5c61112837ff0ae05786023342d8f6b
EuwenAldrichVillarin <euwenaldrich123@gmail.com>
2024-08-14 22:34:28 +0800

    Update Triangle.java

--- Chapter 1/Triangle.java
+++ Chapter 1/Triangle.java
@@ -1,1 +1,14 @@
-
+public class Triangle {
+    public static void main(String[]args){
+        int i = 7;
+        for(int j = 1;j<=i;j++){
+            for (int k = i; k > j; k--) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= (2*j-1); l++) {
+                System.out.print("I");
+            }
+            System.out.println();
+        }
+    }
+}
