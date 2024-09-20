6c594c7d46c2112a6e713960146f12f39ed6ad83
EuwenAldrichVillarin <euwenaldrich123@gmail.com>
2024-09-20 20:56:06 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,17 @@
+public class FirstAddressExcercise{
+   public static void main(String[]args){
+      displayBrand();
+      displayAddress();
+      displayCompany();
+   }
+   public static void displayBrand(){
+      System.out.println("Brand: ASUS");
+   
+   }
+   public static void displayAddress(){
+      System.out.println("Address: No. 15, Lide Road Beitou District\nTaipei City, Taipei 112 TW");
+   }
+   public static void displayCompany(){
+      System.out.println("Company: ASUSTeK Computer Inc.");
+   }
+}
