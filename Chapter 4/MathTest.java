c650fcea61d33564aba27d93fe106c20c49dfbfe
EuwenAldrichVillarin <euwenaldrich123@gmail.com>
2024-10-02 21:11:48 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,13 @@
+   class MathTest {
+      public static void main(String[] args) {
+         System.out.println("SQRT: "+ Math.sqrt(100));
+         System.out.println("SIN: "+ Math.sin(45));
+         System.out.println("COS: "+ Math.cos(45));
+         System.out.println("FLOOR: "+ Math.floor(90));
+         System.out.println("CEIL: "+ Math.ceil(90));
+         System.out.println("ROUND: "+ Math.round(36.2));
+         System.out.println("MAX: "+ Math.max(2,34));
+         System.out.println("MIN: "+ Math.min(35,102));
+         System.out.println("RANDOM: "+ Math.random());
+    }
+}
